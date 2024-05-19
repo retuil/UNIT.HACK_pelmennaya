00982a2a38a1f1bf796185c4f42817180f42f9a2
Сурганов Денис <surganov.denis@gmail.com>
2024-05-19 19:56:22 +0500

    gaze-tracking

--- main.py
+++ main.py
@@ -4,70 +4,138 @@ from kivy.clock import Clock
 import cv2
 from kivy.app import App
 from kivy.uix.widget import Widget
+from gaze_tracking import GazeTracking
+from scipy.linalg import solve
+from kivy.core.window import Window
 import numpy as np
-
 
 class MainScreen(Widget):
     pass
 
-
 class MyCameraApp(App):
     def build(self):
+        # Установить полноэкранный режим
+        Window.fullscreen = 'auto'
         return MainScreen()
 
-
 class CameraPreview(Image):
-    points = [(100, 100), (1100, 100), (100, 600), (1100, 600)]
-    eye_centers = [0 for _ in range(len(points))]  # Placeholder for eye center offsets
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    is_calibration = True
+    # Установим начальные координаты калибровочных точек, которые будут обновлены при инициализации
+    points = [(30, 30), (Window.width - 30, 30), (30, Window.height - 30), (Window.width - 30, Window.height - 30)]
+    w = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
+    h = [[], [], [], []]
+    h1 = []
+    d = []
+
+    def draw_calibration_point(self, f, point, r=10):
+        cv2.circle(f, point, r, (0, 0, 255), -1)
+
+    def transform_point(self, quadrilateral_points, rectangle_points, inner_point):
+        A = np.array([
+            [quadrilateral_points[0][0], quadrilateral_points[0][1], 1, 0, 0, 0],
+            [0, 0, 0, quadrilateral_points[0][0], quadrilateral_points[0][1], 1],
+            [quadrilateral_points[1][0], quadrilateral_points[1][1], 1, 0, 0, 0],
+            [0, 0, 0, quadrilateral_points[1][0], quadrilateral_points[1][1], 1],
+            [quadrilateral_points[3][0], quadrilateral_points[3][1], 1, 0, 0, 0],
+            [0, 0, 0, quadrilateral_points[3][0], quadrilateral_points[3][1], 1],
+        ])
+
+        b = np.array([
+            rectangle_points[0][0],
+            rectangle_points[0][1],
+            rectangle_points[1][0],
+            rectangle_points[1][1],
+            rectangle_points[3][0],
+            rectangle_points[3][1]
+        ])
+
+        c = solve(A, b)
+
+        x, y, z = c[0] * inner_point[0] + c[1] * inner_point[1] + c[2], c[3] * inner_point[0] + c[4] * inner_point[1] + c[5], 1
+
+        new_x = x / z
+        new_y = y / z
+        return new_x, new_y
 
     def __init__(self, **kwargs):
         super(CameraPreview, self).__init__(**kwargs)
-        #Connect to 0th camera
         self.capture = cv2.VideoCapture(0)
-        #Set drawing interval
+        self.gaze = GazeTracking()
+
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+        # Set drawing interval
         Clock.schedule_interval(self.update, 1.0 / 30)
 
-    #Drawing method to execute at intervals
-    def update(self, dt):
-        #Load frame
-        ret, self.frame = self.capture.read()
-        self.tracker()
+        # Обновить координаты калибровочных точек в зависимости от размеров окна
+        Window.bind(on_resize=self.update_points)
 
-        #Convert to Kivy Texture
+    def update_points(self, instance, width, height):
+        self.points = [(30, 30), (width - 30, 30), (30, height - 30), (width - 30, height - 30)]
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if self.is_calibration:
+            p = self.w[0]
+            t1 = self.gaze.horizontal_ratio()
+            t2 = self.gaze.vertical_ratio()
+            if t1 is None or t2 is None:
+                return
+            self.w.pop(0)
+            self.h[p].append((t1, t2))
+            print(p, self.gaze.horizontal_ratio(), self.gaze.vertical_ratio())
+
+            if len(self.w) == 0:
+                for i in range(4):
+                    t3 = 0
+                    t4 = 0
+                    for l1, l2 in self.h[i]:
+                        t3 += l1
+                        t4 += l2
+                    self.h1.append((t3 / len(self.h[i]), t4 / len(self.h[i])))
+                self.d = []
+                self.is_calibration = False
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def calibration(self, dt):
+        self.draw_calibration_point(self.frame, self.points[self.w[0]])
+
+    def main_action(self, dt):
+        pi0 = (self.gaze.horizontal_ratio(), self.gaze.vertical_ratio())
+        if pi0[0] is not None and pi0[1] is not None:
+            self.d.append(pi0)
+            if len(self.d) > 8:
+                self.d.pop(0)
+            pi = (sum(map(lambda x: x[0], self.d)) / len(self.d), sum(map(lambda x: x[1], self.d)) / len(self.d))
+            x0, y0 = self.transform_point(self.h1, self.points, pi)
+            print(pi, (x0, y0))
+
+            self.draw_calibration_point(self.frame, (int(x0), int(y0)), 25)
+
+        left_pupil = self.gaze.pupil_left_coords()
+        right_pupil = self.gaze.pupil_right_coords()
+        cv2.putText(self.frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(self.frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+    def update(self, dt):
+        _, self.frame = self.capture.read()
+        self.gaze.refresh(self.frame)
+
+        self.frame = self.gaze.annotated_frame()
+        self.frame = cv2.resize(self.frame, (Window.width, Window.height))
+
+        if self.is_calibration:
+            self.calibration(dt)
+        else:
+            self.main_action(dt)
+
         buf = cv2.flip(self.frame, 0).tostring()
         texture = Texture.create(size=(self.frame.shape[1], self.frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-        #Change the texture of the instance
         self.texture = texture
-
-    def get_eye_center(self, roi_gray):
-        eyes = self.eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            return (ex + ew // 2, ey + eh // 2)
-        return None
-
-    def tracker(self):
-        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-
-        # Детекция глаз
-        eyes = self.eye_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-
-        for (x, y, w, h) in eyes:
-            x1, y1, w1, h1 = x - w // 6, y + h // 4, w + 2 * (w // 6), h - 2 * (h // 4)
-
-            # Область глаза
-            eye_roi = self.frame[y1: y1 + h1, x1: x1 + w1]
-
-            # Нахождение самого темного пикселя внутри глаза
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(cv2.cvtColor(eye_roi, cv2.COLOR_BGR2GRAY))
-            if min_loc[0] < 2 or min_loc[1] < 2 or min_loc[0] > w - 2 or min_loc[1] > h - 2:
-                continue
-            # Отображение зрачка как красная точка
-
-            cv2.rectangle(self.frame, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 0), 2)
-            cv2.circle(self.frame, (int(min_loc[0] + x1), int(min_loc[1] + y1)), 2, (0, 0, 255), 2)
 
 if __name__ == '__main__':
     MyCameraApp().run()
